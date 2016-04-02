5d997a187b90072eead662f13e9ea0c326fe7550
Junjie Li <jj.muyang@gmail.com>
2016-04-01 19:20:37 -0500

    using method without loop

--- Junjie Li/isPowerOfThree.java
+++ Junjie Li/isPowerOfThree.java
@@ -8,11 +8,14 @@ Could you do it without using any loop / recursion?
 public class Solution {
     public boolean isPowerOfThree(int n) {
 
+        return (Math.log10(n) / Math.log10(3)) % 1 == 0;
+        /*
         if (n > 1){
             while (n % 3 == 0) {
                 n /= 3;
             }
         }
         return n == 1;
+        */
     }
 }
